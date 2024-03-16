3e1115106ef4ef182498a6e3e4365e37198cace7
ywyjds <163621528+ywyjds@users.noreply.github.com>
2024-03-16 14:42:55 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          yang wen chat 2.0
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          get ywi3.02
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
